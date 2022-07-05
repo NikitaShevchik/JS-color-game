8b6278f84aabf27e94a80c34e7137a2e18b04cd7
NikitaShevchik <shevchik.nikita@gmail.com>
2022-07-05 13:25:39 +0300

    events for click

--- js/scrypt.js
+++ js/scrypt.js
@@ -2,7 +2,7 @@
 const mainBoard = document.querySelector('.items')
 
 let rows = 5;
-let cols = 15;
+let cols = 5;
 
 let rowsHard = 8; // усложненный уровень
 let colsHard = 8; // усложненный уровень
@@ -16,12 +16,12 @@ var lastColorsArrayIndex = 0;
 
 // Генератор игрового поля. Ряды, колонки, массив цветов
 function generationBoard(row, col, colorArr) {
-    for (let i = 0; i < col; i++) {
+    for (let i = 0; i < row; i++) {
         mainBoard.innerHTML += `<div class="items__cols"></div>`
     }
     const allColumns = mainBoard.querySelectorAll('.items__cols');
     for (let k of allColumns) {
-        for (let i = 0; i < row; i++) {
+        for (let i = 0; i < col; i++) {
             k.innerHTML += `<div class="items__item"></div>`
         }
     }
@@ -34,7 +34,35 @@ function generationBoard(row, col, colorArr) {
         }
     }
     randomColor(colorArr)
+
+
+    function listenerForItems(arrayColors) {
+        const allItems = document.querySelectorAll('.items__item');
+        for (let item of allItems) {
+            item.addEventListener('click', () => changeColor(arrayColors, item))
+        }
+    }
+    function changeColor(arr, target) {
+        for (let i = 0; i <= lastColorsArrayIndex; i++) {
+            if (target.classList.contains(arr[i])) {
+                if (i < lastColorsArrayIndex) {
+                    target.classList.remove(arr[i]);
+                    target.classList.add(arr[i + 1])
+                } else if (i == lastColorsArrayIndex) {
+                    target.classList.remove(arr[i]);
+                    i = 0;
+                    target.classList.add(arr[i]);
+                }
+                break
+            }
+        }
+    }
+    listenerForItems(colorArr)
 }
 generationBoard(rows, cols, colors)
 
 // ахахахахахах я перепутал колонки и ряды, Никита из завтра - исправить!!!
+// есть!
+
+
+
